4ad0b3715b100953abd451dc00c5fa60bb405a9e
rifkyhilman <rifkyhilman265@gmail.com>
2024-03-25 11:44:24 +0700

    :heavy_plus_sign: Add bootstrap 5
    add bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-createApp(App).use(router).mount('#app')
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootsrtap from 'bootstrap/dist/js/bootstrap.bundle'
+
+
+createApp(App).use(router).use(bootsrtap).mount('#app')
