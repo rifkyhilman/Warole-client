c277ae9cbc05432da69e7aee2f39ab8c27e8b09b
rifkyhilman <rifkyhilman265@gmail.com>
2024-03-27 08:23:27 +0700

    :construction: Add field form register
    add field form register

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -2,52 +2,52 @@
 <template>
     <div id="page-wrap">
         <div class="from">
-            <form class="row g-3">
+            <form class="row g-3" @submit.prevent="handleSubmit">
                 <span id="form-title">Register</span>
                 <div class="col-md-6">
                     <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                    <input v-model="inputEmail" type="email" class="form-control" id="inputEmail4" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <input v-model="inputPassword" type="password" class="form-control" id="inputPassword4" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputName" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="inputName">
+                    <input v-model="inputName" type="text" class="form-control" id="inputName" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputPhone" class="form-label">PhoneNumber</label>
-                    <input type="text" class="form-control" id="inputPhone">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">FullAddress</label>
-                    <input type="text" class="form-control" id="inputAddress">
+                    <input v-model="inputPhone" type="number" class="form-control" id="inputPhone" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputProvinci" class="form-label"> Province </label>
-                    <input type="text" class="form-control" id="inputProvince">
+                    <input v-model="inputProvince" type="text" class="form-control" id="inputProvince" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
+                    <input v-model="inputCity" type="text" class="form-control" id="inputCity" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputSubdistrict" class="form-label"> Subdistrict </label>
-                    <input type="text" class="form-control" id="inputSubdistrict">
+                    <input v-model="inputSubdistrict" type="text" class="form-control" id="inputSubdistrict" required>
                 </div> 
                 <div class="col-md-6">
                     <label for="inputWard" class="form-label"> Ward </label>
-                    <input type="text" class="form-control" id="inputWard">
+                    <input v-model="inputWard" type="text" class="form-control" id="inputWard" required>
                 </div>
                 <div class="col-12">
+                    <label for="inputAddress" class="form-label">FullAddress</label>
+                    <input v-model="inputFulladress" type="text" class="form-control" id="inputAddress" required>
+                </div>
+                <!-- <div class="col-12">
                     <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="gridCheck">
                     <label class="form-check-label" for="gridCheck">
                         Check me out
                     </label>
                     </div>
-                </div>
+                </div> -->
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Register</button>
                 </div>
@@ -58,26 +58,32 @@
 
 <script>
 export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Register',
     data() {
         return {
-            username: null,
-            email: null,
-            password: null,
-            region: null,
-            phone: null
+            inputName: null,
+            inputEmail: null,
+            inputPassword: null,
+            inputPhone: null,
+            inputFulladress: null,
+            inputProvince: null,
+            inputCity: null,
+            inputSubdistrict: null,
+            inputWard: null
         }
     },
     methods: {
         async handleSubmit() {
             try {
                 const data = {
-                    name: this.username,
-                    email: this.email,
-                    password: this.password,
-                    region: this.region,
-                    phone: this.phone.toString()
+                    name: this.inputName,
+                    email: this.inputEmail,
+                    password: this.inputPassword,
+                    phoneNumber: this.inputPhone.toString(),
+                    fullAddress: this.inputFulladress,
+                    provinsi: this.inputProvince,
+                    kota: this.inputCity,
+                    kecamatan: this.inputSubdistrict,
+                    kelurahan: this.inputWard
                 };
            
                 const response = await fetch('https://ecoms.zeabur.app/api/v1/auth/register', {
