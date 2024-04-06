0e51e56d69ac8176792243447cf2242968a17c42
rifkyhilman <rifkyhilman265@gmail.com>
2024-04-06 17:22:31 +0700

    :heavy_plus_sign: Add bootstrapIcons use npm
    add bootstrapIcons use npm

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootsrtap from 'bootstrap/dist/js/bootstrap.bundle'
-
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).use(router).use(bootsrtap).mount('#app')
